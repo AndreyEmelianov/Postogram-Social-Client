c3bf5c671e72f0f41c28c056e61d6f290da586bc
Andrey Emelianov <621077@mail.ru>
2024-03-07 12:24:28 +0300

    сконфигурировал  baseQuery и сделал базовую настройку api

---
+++ src/app/services/api.ts
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query"
+
+import { BASE_URL } from "../../constants"
+import type { RootState } from "../store"
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: `${BASE_URL}/api`,
+  prepareHeaders: (headers, { getState }) => {
+    const token =
+      (getState() as RootState).auth.token ||
+      localStorage.getItem("postogram-token")
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`)
+    }
+
+    return headers
+  },
+})
+
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 2 })
+
+export const api = createApi({
+  reducerPath: "splitApi",
+  baseQuery: baseQueryWithRetry,
+  refetchOnMountOrArgChange: true,
+  endpoints: () => ({}),
+})
